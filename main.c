98258ce97c9ae179f521bbe959c794d12a59d328
junior-parreirabr <79853912+junior-parreirabr@users.noreply.github.com>
2021-08-14 17:12:07 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * TP01_176888.c
+ *
+ * Created: 14/08/2021 15:54:34
+ * Author : Aguinaldo
+ */ 
+
+#include <avr/io.h>
+extern void testa();
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		testa();
+    }
+}
+
